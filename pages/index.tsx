497a9ee04b93d5592ed974861944a66585258967
Julien Ferez <Julienferez@icloud.com>
2022-04-11 18:22:43 +0200

    Auth0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { UserProvider, useUser } from "@auth0/nextjs-auth0";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,27 +6,55 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <div>
-        <Link href="/onlineStore" passHref={true}>
-          <Image
-            width={500}
-            height={500}
-            src="/images/tutoEnLigne"
-            alt="tutoenligne"
-          />
-        </Link>
-        {/* <Link href="">
+export default function Home({ children }) {
+  const { user, error, isLoading } = useUser();
+  if (user) {
+    return (
+      <>
+        <Navbar />
+        <div>
+          <div>Welcome{user.name}</div>
+          <Link href="/onlineStore" passHref={true}>
+            <Image
+              width={500}
+              height={500}
+              src="/images/tutoEnLigne.png"
+              alt="tutoenligne"
+            />
+          </Link>
+          {/* <Link href="">
           <Image src="coursdebricolagemag" alt="" />
         </Link>
         <Link href="">
           <Image src="assistancetel" alt="" />
         </Link> */}
-      </div>
-      <Footer />
-    </>
-  );
+        </div>
+        <Footer />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Navbar />
+
+        <div>
+          <Link href="/onlineStore" passHref={true}>
+            <Image
+              width={500}
+              height={500}
+              src="/images/tutoEnLigne.png"
+              alt="tutoenligne"
+            />
+          </Link>
+          {/* <Link href="">
+          <Image src="coursdebricolagemag" alt="" />
+        </Link>
+        <Link href="">
+          <Image src="assistancetel" alt="" />
+        </Link> */}
+        </div>
+        <Footer />
+      </>
+    );
+  }
 }
