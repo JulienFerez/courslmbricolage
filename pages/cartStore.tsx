380b78298856d8bacc6225140736d83de11981d5
Nordine El yazidi <78361263+nordine-yzd@users.noreply.github.com>
2022-04-20 17:33:31 +0200

    Update cartStore.tsx

--- pages/cartStore.tsx
+++ pages/cartStore.tsx
@@ -19,7 +19,6 @@ export const getServerSideProps: GetServerSideProps = async ({
     .toArray();
 
   const panierString = await JSON.parse(JSON.stringify(panier));
-  console.log(panierString[0].panier);
 
   return {
     props: {
